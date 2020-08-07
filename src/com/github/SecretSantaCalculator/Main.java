9500a5f74c1dec6285f8b6dd8187f2544937ffb7
Avery Panola <lucastloftier@gmail.com>
2020-08-07 10:54:15 -0500

    Added basic non-functional GUI

--- src/com/github/SecretSantaCalculator/Main.java
+++ src/com/github/SecretSantaCalculator/Main.java
@@ -2,18 +2,6 @@ package com.github.SecretSantaCalculator;
 
 public class Main {
     public static void main(String[] args) {
-        SecretSanta secretSanta = new SecretSanta();
-        secretSanta.addName("This");
-        secretSanta.addName("Might");
-        secretSanta.addName("Actually");
-        secretSanta.addName("Work");
-        secretSanta.addName("But");
-        secretSanta.addName("Probably");
-        secretSanta.addName("Not");
-        secretSanta.addName("You");
-        secretSanta.addName("Know");
-        System.out.println(secretSanta.partnerData);
-        secretSanta.calculatePartners();
-        System.out.println(secretSanta.calculatePartners().showPartnerData());
+        new MainGUI();
     }
 }
--- src/com/github/SecretSantaCalculator/MainGUI.java
+++ src/com/github/SecretSantaCalculator/MainGUI.java
@@ -1,5 +1,25 @@
 package com.github.SecretSantaCalculator;
 
-public class MainGUI {
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import java.awt.*;
 
+import static java.awt.Color.*;
+
+public class MainGUI {
+    public MainGUI() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JLabel mainTitle = new JLabel("Secret Santa");
+
+        panel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED, black, blue));
+        panel.setLayout(new GridLayout());
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.add(mainTitle);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Secret Santa");
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
